c4de196b2f2586385f4f1563d35e3b740db30a23
your_github_username <r.lenartowicz@icloud.com>
2022-06-07 14:43:48 +0000

    Final canges

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,43 @@
+
+'''
+Final Project
+'''
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2022-06-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    '''English to French translator.'''
+    #write the code here
+    translation = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    french_text = translation["translations"][0]["translation"]
+    return french_text
+
+def french_to_english(french_text):
+    '''French to English ytanslator. '''
+    #write the code here
+    translation = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    english_text = translation["translations"][0]["translation"]
+    return english_text
+
+print(french_to_english("Hello"))
+
+print(english_to_french("Bonjour"))
